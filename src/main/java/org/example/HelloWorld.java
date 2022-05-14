04c3a1430fb3f4ee378e2221a7f680f3426d5292
root <2287071560@qq.com>
2022-05-14 10:18:27 +0800

    田径666

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("你好");
         System.out.println("我是田径777");
+        System.out.println("我是田径666");
     }
 
 
